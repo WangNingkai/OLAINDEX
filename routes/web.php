ca21c96a4de0f9c15dc46de9dabc76ac69d5e789
WangNingkai <1655586865@qq.com>
2018-10-09 16:28:17 +0800

    路由注释

--- routes/web.php
+++ routes/web.php
@@ -11,9 +11,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+// 授权、刷新Token
 Route::get('/oauth', 'OauthController@oauth')->name('oauth');
 Route::get('/refresh', 'OauthController@refreshToken')->name('refresh');
 
+// 目录显示
 Route::get('/', 'GraphFetchController@oneFetchItemList');
 Route::get('/list/{path?}', 'GraphFetchController@oneFetchItemList')->name('list');
 Route::get('/item/{itemId}', 'GraphFetchController@oneShowItem')->name('item');
@@ -23,15 +25,16 @@ Route::get('/item/thumb/{itemId}', 'GraphFetchController@oneFetchThumb')->name('
 Route::get('/item/view/{itemId}', 'GraphFetchController@oneFetchView')->name('view')->middleware('throttle:10,2');
 Route::get('/item/origin/view/{itemId}', 'GraphFetchController@oneFetchDownload')->name('origin.view');
 
+// 图床
 Route::get('/image', 'GraphPostController@uploadImage')->name('image')->middleware('checkImage');
 Route::post('/image/upload', 'GraphPostController@uploadImage')->name('image.upload')->middleware('throttle:10,2','checkImage');
 Route::get('/item/delete/{itemId}', 'GraphPostController@deleteItem')->name('delete')->middleware('checkImage');
 
+// 后台小文件上传
 Route::get('/admin/file', 'GraphPostController@uploadFile')->name('admin.file')->middleware('checkAuth');
 Route::post('/admin/file/upload', 'GraphPostController@uploadFile')->name('admin.file.upload')->middleware('checkAuth','throttle:10,2');
 
-Route::get('/search', 'GraphFetchController@oneSearchItem')->name('search')->middleware('throttle:10,1');
-
+// 后台管理
 Route::any('/login', 'ManageController@login')->name('login');
 Route::any('/admin', 'ManageController@basic')->name('admin.basic');
 Route::any('/admin/show', 'ManageController@show')->name('admin.show');
