1e7d10c46fd18d3b300175050d8646a430fdee2d
WangNingkai <1655586865@qq.com>
2018-10-15 14:21:45 +0800

    remove ua handle

--- app/Http/Middleware/HotlinkProtection.php
+++ app/Http/Middleware/HotlinkProtection.php
@@ -25,17 +25,6 @@ class HotlinkProtection
         if (!$request->server('HTTP_REFERER')) {
             abort(403);
         }
-        $ua = $request->server('HTTP_USER_AGENT');
-        $badUA= ['Googlebot-Image','FeedDemon ','BOT/0.1 (BOT for JCE)','CrawlDaddy ','Java','Feedly','UniversalFeedParser','ApacheBench','Swiftbot','ZmEu','Indy Library','oBot','jaunty','YandexBot','AhrefsBot','MJ12bot','WinHttp','EasouSpider','HttpClient','Microsoft URL Control','YYSpider','jaunty','Python-urllib','lightDeckReports Bot','PHP','vxiaotou-spider','spider'];
-        if(!$ua) {
-            abort(403);
-        }else{
-            foreach ($badUA as $item) {
-                if(str_contains($ua, $item)) {
-                    abort(403);
-                }
-            }
-        }
         //判断$_SERVER['HTTP_REFERER'] 是不是处于白名单
         foreach ($whiteList as $item) {
             if(strpos($request->server('HTTP_REFERER'),$item) == 0) {
