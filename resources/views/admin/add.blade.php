cfdb300d052765aeb7deab98b64b1457cbc95ff5
WangNingkai <1655586865@qq.com>
2018-10-20 20:16:35 +0800

    .

--- resources/views/admin/add.blade.php
+++ resources/views/admin/add.blade.php
@@ -24,6 +24,7 @@
     <script>
         $(function () {
             var mdeditor = new InscrybMDE({
+                autoDownloadFontAwesome: false,
                 autofocus: true,
                 autosave: {
                     enabled: false,
