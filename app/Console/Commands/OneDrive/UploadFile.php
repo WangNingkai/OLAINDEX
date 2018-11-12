97f430bb9d3e2f06feac17d5b8da9a6e52f61f5d
WangNingkai <1655586865@qq.com>
2018-11-12 14:29:07 +0800

    upload fix

--- app/Console/Commands/OneDrive/UploadFile.php
+++ app/Console/Commands/OneDrive/UploadFile.php
@@ -45,7 +45,7 @@ class UploadFile extends Command
         }
         $local = $this->argument('local');
         $remote = $this->argument('remote');
-        $chuck = $this->argument('chuck');
+        $chuck = $this->option('chuck');
         $file_size = Tool::readFileSize($local);
         $this->info('开始上传...');
         if ($file_size < 10485760) {
