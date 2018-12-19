b019f5530c2257ca0c11429aabed4dcf182107fd
WangNingkai <1655586865@qq.com>
2018-12-19 16:59:39 +0800

    opt

--- app/Http/Middleware/HotlinkProtection.php
+++ app/Http/Middleware/HotlinkProtection.php
@@ -21,16 +21,11 @@ class HotlinkProtection
         if ($hotlink_protection) {
             // 简单处理防盗链，建议加入更加其他防盗链措施
             $whiteList = explode(' ', $hotlink_protection);
-            if (!$request->server('HTTP_REFERER')) {
-                abort(403);
-            }
+            abort_unless(403, $request->server('HTTP_REFERER'));
             //判断 $_SERVER['HTTP_REFERER'] 是不是处于白名单
             foreach ($whiteList as $item) {
-                if (strpos($request->server('HTTP_REFERER'), $item) === 0) {
-                    return $next($request);
-                } else {
-                    abort(403);
-                }
+                abort_unless(403, strpos($request->server('HTTP_REFERER'), $item) === 0);
+                return $next($request);
             }
         }
 
