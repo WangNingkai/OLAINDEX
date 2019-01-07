6ca344129778d82d746de26be68ecf3f021a978f
WangNingkai <1655586865@qq.com>
2019-01-07 15:47:03 +0800

    安装更新提示说明

--- resources/views/default/install/init.blade.php
+++ resources/views/default/install/init.blade.php
@@ -60,7 +60,7 @@
                     <label class="form-control-label" for="redirect_uri">redirect_uri </label>
                     <input type="text" class="form-control" id="redirect_uri" name="redirect_uri"
                            value="{{ trim(config('app.url'),'/').'/oauth' }}">
-                    <span class="form-text text-danger">确保回调地址格式为此形式 http(s)://you.domain/oauth，使用中转域名无需https协议 </span>
+                    <span class="form-text text-danger">确保回调地址格式为此形式 http(s)://you.domain/oauth，使用中转域名无需https协议（注意：如果通过CDN开启HTTPS而非配置SSL证书，部分回调CDN会跳转http地址，从而导致申请失败） </span>
                 </div>
                 <div class="form-group">
                     <label class="form-control-label" for="client_id"><b>client_id</b></label>
