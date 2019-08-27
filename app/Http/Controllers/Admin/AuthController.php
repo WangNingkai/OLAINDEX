a62ffdc98aa4423f4d663a4c6790cd1c06b2d4be
dongdongGit <1697919363@qq.com>
2019-08-27 15:47:51 +0800

    update: admin login view

--- app/Http/Controllers/Admin/AuthController.php
+++ app/Http/Controllers/Admin/AuthController.php
@@ -33,7 +33,7 @@ class AuthController extends Controller
      */
     public function showLoginForm()
     {
-        return view(config('olaindex.theme') . 'admin.login');
+        return view(config('olaindex.theme') . 'admin.login-v2');
     }
 
     public function authenticated(Request $request, $user)
