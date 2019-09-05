0859c141009d524806203e04ad3d0f1258fc6c8c
dongdongGit <1697919363@qq.com>
2019-09-05 15:18:13 +0800

    remove function convertUnit

--- app/Helpers.php
+++ app/Helpers.php
@@ -9,7 +9,6 @@ use App\Models\OneDrive;
 use Illuminate\Support\Arr;
 use App\Models\Admin;
 use Illuminate\Support\Facades\Redis;
-use Illuminate\Support\Str;
 
 if (!function_exists('convertSize')) {
     /**
@@ -361,20 +360,5 @@ if (!function_exists('clearOnedriveCache')) {
         $redis = Redis::connection('cache');
         $caches = $redis->keys(config('cache.prefix') . ':one_' . $id . '*');
         $redis->del($caches);
-    }
-}
-
-if (!function_exists('convertUnit')) {
-    function convertUnit($standard, $conver)
-    {
-        $units = ['B', 'KB', 'MB', 'GB', 'TB'];
-        $standard_unit = explode(' ', $standard);
-        $conver_unit = explode(' ', $conver);
-        $flip_units = array_flip($units);
-        $standard_index = $flip_units[$standard_unit[1]];
-        $conver_index = $flip_units[$conver_unit[1]];
-        $sub = $conver_index - $standard_index;
-
-        return $sub >= 0 ? $conver_unit[0] * pow(1024, $sub) : round($conver_unit[0] / pow(1024, abs($sub)), 2);
     }
 }
