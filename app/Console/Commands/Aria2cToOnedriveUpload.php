f5729627a616de632cedf26433fbdabb095feceb
dongdongGit <1697919363@qq.com>
2019-10-14 10:43:56 +0800

    删除多余代码

--- app/Console/Commands/Aria2cToOnedriveUpload.php
+++ app/Console/Commands/Aria2cToOnedriveUpload.php
@@ -53,7 +53,6 @@ class Aria2cToOnedriveUpload extends Command
         }
 
         getDefaultOneDriveAccount();
-        $ext = pathinfo($path, PATHINFO_EXTENSION);
 
         if (is_file($path)) {
             $target = Arr::last(explode('/', pathinfo($path, PATHINFO_DIRNAME)), null, '/upload') . '/' . pathinfo($path, PATHINFO_FILENAME);
