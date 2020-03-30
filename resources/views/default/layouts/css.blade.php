a358f3f4e9b19e3ca4899f78c3b6af8371541385
WangNingkai <1655586865@qq.com>
2020-03-30 09:23:33 +0800

    opt blade

---
+++ resources/views/default/layouts/css.blade.php
@@ -0,0 +1,7 @@
+<link href="https://cdn.staticfile.org/bootswatch/4.4.1/{{ setting('theme','lux') }}/bootstrap.min.css"
+      rel="stylesheet">
+<link href="https://cdn.jsdelivr.net/npm/remixicon@2.3.0/fonts/remixicon.css" rel="stylesheet">
+<link rel="stylesheet" href="https://cdn.staticfile.org/font-awesome/4.7.0/css/font-awesome.css">
+<link rel="stylesheet" href="https://cdn.staticfile.org/limonte-sweetalert2/7.33.1/sweetalert2.min.css">
+<link rel="stylesheet" href="https://cdn.staticfile.org/github-markdown-css/4.0.0/github-markdown.min.css">
+<link rel="stylesheet" href="https://cdn.staticfile.org/fancybox/3.5.7/jquery.fancybox.min.css">
--- resources/views/default/layouts/main.blade.php
+++ resources/views/default/layouts/main.blade.php
@@ -9,19 +9,14 @@
     <meta name="keywords" content="OLAINDEX,OneDrive,Index,Microsoft OneDrive,Directory Index"/>
     <meta name="description" content="OLAINDEX,Another OneDrive Directory Index"/>
     <meta name="csrf-token" content="{{ csrf_token() }}">
-    <link href="https://cdn.staticfile.org/bootswatch/4.4.1/{{ setting('theme','lux') }}/bootstrap.min.css"
-          rel="stylesheet">
-    <link href="https://cdn.jsdelivr.net/npm/remixicon@2.3.0/fonts/remixicon.css" rel="stylesheet">
-    <link rel="stylesheet" href="https://cdn.staticfile.org/font-awesome/4.7.0/css/font-awesome.css">
-    <link rel="stylesheet" href="https://cdn.staticfile.org/limonte-sweetalert2/7.33.1/sweetalert2.min.css">
-    <link rel="stylesheet" href="https://cdn.staticfile.org/github-markdown-css/4.0.0/github-markdown.min.css">
-    <link rel="stylesheet" href="https://cdn.staticfile.org/fancybox/3.5.7/jquery.fancybox.min.css">
-
-    @yield('css')
+    @section('css')
+        @include('default.layouts.css')
+    @show
     <style>
         * {
             outline-style: none
         }
+
         .item-list .list-group-item {
             border: 0;
         }
@@ -30,7 +25,7 @@
         App = {
             'routes': {},
             '_token': '{{ csrf_token() }}',
-        };
+        }
     </script>
 </head>
 
@@ -82,49 +77,44 @@
         </div>
     </footer>
 </div>
-<script src="https://cdn.staticfile.org/jquery/3.4.1/jquery.min.js"></script>
-<script src="https://cdn.staticfile.org/popper.js/1.16.1/umd/popper.min.js"></script>
-<script src="https://cdn.staticfile.org/twitter-bootstrap/4.4.1/js/bootstrap.min.js"></script>
-<script src="https://cdn.staticfile.org/limonte-sweetalert2/7.33.1/sweetalert2.all.min.js"></script>
-<script src="https://cdn.staticfile.org/jquery.lazyload/1.9.1/jquery.lazyload.min.js"></script>
-<script src="https://cdn.staticfile.org/fancybox/3.5.7/jquery.fancybox.min.js"></script>
-<script src="https://cdn.staticfile.org/clipboard.js/2.0.6/clipboard.min.js"></script>
-@yield('js')
+@section('js')
+    @include('default.layouts.js')
+@show
 {!! setting('statistics') !!}
 <script>
-    $(function () {
+    $(function() {
         $('[data-fancybox="image-list"]').fancybox({
-            type: "image",
+            type: 'image',
             thumbs: {
                 autoStart: true,
-                axis: 'x'
+                axis: 'x',
             },
             buttons: [
-                "zoom",
-                "slideShow",
-                "fullScreen",
-                "download",
-                "thumbs",
-                "close"
+                'zoom',
+                'slideShow',
+                'fullScreen',
+                'download',
+                'thumbs',
+                'close',
             ],
-        });
-        let clipboard = new ClipboardJS('.clipboard');
-        clipboard.on('success', function (e) {
-            console.info('Action:', e.action);
-            console.info('Text:', e.text);
-            console.info('Trigger:', e.trigger);
-            e.clearSelection();
-        });
-        clipboard.on('error', function (e) {
-            console.error('Action:', e.action);
-            console.error('Trigger:', e.trigger);
-        });
+        })
+        let clipboard = new ClipboardJS('.clipboard')
+        clipboard.on('success', function(e) {
+            console.info('Action:', e.action)
+            console.info('Text:', e.text)
+            console.info('Trigger:', e.trigger)
+            e.clearSelection()
+        })
+        clipboard.on('error', function(e) {
+            console.error('Action:', e.action)
+            console.error('Trigger:', e.trigger)
+        })
         $('[data-toggle="tooltip"]').tooltip({
             title: '已复制',
-            trigger: 'click'
-        });
-        $('img.lazy').lazyload();
-    });
+            trigger: 'click',
+        })
+        $('img.lazy').lazyload()
+    })
 </script>
 </body>
 
