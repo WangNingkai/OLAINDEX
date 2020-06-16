c325ffc89ebe674fa45dad9bb6454dbd0d8dc250
WangNingkai <1655586865@qq.com>
2020-06-16 15:30:23 +0800

    complete components

--- resources/views/default/one.blade.php
+++ resources/views/default/one.blade.php
@@ -7,51 +7,14 @@
             <button id="btnChoiceAccount" type="button" class="btn btn-primary btn-sm dropdown-toggle"
                     data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"></button>
             <div class="dropdown-menu" aria-labelledby="btnChoiceAccount">
-                @foreach($accounts as $account)
+                @foreach($accounts as $key => $account)
                     <a class="dropdown-item"
-                       href="{{ route('drive',['hash' => $account['hash_id']]) }}">{{ $account['remark'] }}</a>
+                       href="{{ route('drive',['hash' => $account['hash_id']]) }}">{{ $key + 1 .':'.$account['remark'] }}</a>
                 @endforeach
             </div>
         </div>
     </div>
-    <nav aria-label="breadcrumb" class="d-none d-md-block d-md-none">
-        <ol class="breadcrumb">
-            <li class="breadcrumb-item"><a href="{{ route('drive.query', ['hash' => $hash]) }}"><i
-                        class="ri-home-fill"></i> Home</a></li>
-            @if(!blank($path))
-                @if (count($path) < 5)
-                    @foreach ($path as $key => $value)
-                        @if(end($path) === $value && $key === (count($path) - 1))
-                            <li class="breadcrumb-item active">{{ str_limit($value,20)  }}</li>
-                        @else
-                            @if (!blank($value))
-                                <li class="breadcrumb-item ">
-                                    <a href="{{ route('drive.query', ['hash' => $hash, 'query' => url_encode(\App\Helpers\Tool::combineBreadcrumb($key + 1, $path))]) }}">
-                                        {{  str_limit($value,20) }}
-                                    </a>
-                                </li>
-                            @endif
-                        @endif
-                    @endforeach
-                @else
-                    <li class="breadcrumb-item active"> ...</li>
-                    @foreach ($path as $key => $value)
-                        @if(end($path) === $value && $key === (count($path) - 1))
-                            <li class="breadcrumb-item active">{{  str_limit($value,20)  }}</li>
-                        @else
-                            @if (!blank($value) && $key === (count($path) - 2))
-                                <li class="breadcrumb-item ">
-                                    <a href="{{ route('drive.query', ['hash' => $hash, 'query' => url_encode(\App\Helpers\Tool::combineBreadcrumb($key + 1, $path))]) }}">
-                                        {{  str_limit($value,20) }}
-                                    </a>
-                                </li>
-                            @endif
-                        @endif
-                    @endforeach
-                @endif
-            @endif
-        </ol>
-    </nav>
+    @include('default.components.breadcrumb',['hash' => $hash, 'path' => $path])
     @if (!blank($doc['head']))
         <div class="card border-light mb-3">
             <div class="card-header"><i class="ri-send-plane-fill"></i> HEAD</div>
@@ -93,7 +56,13 @@
                             </td>
                             <td>{{ date('M d H:i', strtotime($data['lastModifiedDateTime'])) }}</td>
                             <td class="d-none d-md-block d-md-none">{{ convert_size($data['size']) }}</td>
-                            <td></td>
+                            <td>
+                                @if(! array_has($item,'folder'))
+                                    <a href="#"><i class="ri-folder-open-fill"></i></a>
+                                @else
+                                    <a href="#"><i class="ri-download-fill"></i></a>
+                                @endif
+                            </td>
                         </tr>
                     @endforeach
                 @endif
--- resources/views/default/preview.blade.php
+++ resources/views/default/preview.blade.php
@@ -1,57 +1,7 @@
 @extends('default.layouts.main')
 @section('title','OLAINDEX')
 @section('content')
-    <div class="btn-group mb-3" role="group" aria-label="choiceAccount">
-        <button type="button" class="btn btn-primary btn-sm">网盘列表</button>
-        <div class="btn-group" role="group">
-            <button id="btnChoiceAccount" type="button" class="btn btn-primary btn-sm dropdown-toggle"
-                    data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"></button>
-            <div class="dropdown-menu" aria-labelledby="btnChoiceAccount">
-                @foreach($accounts as $account)
-                    <a class="dropdown-item"
-                       href="{{ route('drive',['hash' => $account['hash_id']]) }}">{{ $account['remark'] }}</a>
-                @endforeach
-            </div>
-        </div>
-    </div>
-    <nav aria-label="breadcrumb" class="d-none d-md-block d-md-none">
-        <ol class="breadcrumb">
-            <li class="breadcrumb-item"><a href="{{ route('drive.query', ['hash' => $hash]) }}"><i
-                        class="ri-home-fill"></i> Home</a></li>
-            @if(!blank($path))
-                @if (count($path) < 5)
-                    @foreach ($path as $key => $value)
-                        @if(end($path) === $value && $key === (count($path) - 1))
-                            <li class="breadcrumb-item active">{{ str_limit($value,20)  }}</li>
-                        @else
-                            @if (!blank($value))
-                                <li class="breadcrumb-item ">
-                                    <a href="{{ route('drive.query', ['hash' => $hash, 'query' => url_encode(\App\Helpers\Tool::combineBreadcrumb($key + 1, $path))]) }}">
-                                        {{  str_limit($value,20) }}
-                                    </a>
-                                </li>
-                            @endif
-                        @endif
-                    @endforeach
-                @else
-                    <li class="breadcrumb-item active"> ...</li>
-                    @foreach ($path as $key => $value)
-                        @if(end($path) === $value && $key === (count($path) - 1))
-                            <li class="breadcrumb-item active">{{  str_limit($value,20)  }}</li>
-                        @else
-                            @if (!blank($value) && $key === (count($path) - 2))
-                                <li class="breadcrumb-item ">
-                                    <a href="{{ route('drive.query', ['hash' => $hash, 'query' => url_encode(\App\Helpers\Tool::combineBreadcrumb($key + 1, $path))]) }}">
-                                        {{  str_limit($value,20) }}
-                                    </a>
-                                </li>
-                            @endif
-                        @endif
-                    @endforeach
-                @endif
-            @endif
-        </ol>
-    </nav>
+    @include('default.components.breadcrumb',['hash' => $hash, 'path' => $path])
     <div class="card border-light mb-3">
         <div class="card-header">{{ $file['name'] }}</div>
         <div class="card-body">
@@ -62,7 +12,7 @@
                 <div class="input-group mb-3">
                     <label for="link1"></label>
                     <input type="text" id="link1" class="form-control"
-                           value="{{ route('drive.query', ['hash' => $hash, 'query' => url_encode(implode('/', $path))]) }}">
+                           value="{{ route('drive.query', ['hash' => $hash, 'query' => url_encode(implode('/', $path)),'download' => 1]) }}">
                     <div class="input-group-append">
                         <a href="javascript:void(0)" style="text-decoration: none" data-toggle="tooltip"
                            data-placement="right" data-clipboard-target="#link1" class="clipboard"><span
@@ -70,7 +20,9 @@
                     </div>
                 </div>
             </div>
-            <div class="text-center"><a href="#" class="btn btn-success"><i class="fa fa-download"></i> 下载</a></div>
+            <div class="text-center"><a
+                    href="{{ route('drive.query', ['hash' => $hash, 'query' => url_encode(implode('/', $path)),'download' => 1]) }}"
+                    class="btn btn-success"><i class="fa fa-download"></i> 下载</a></div>
         </div>
     </div>
 @stop
