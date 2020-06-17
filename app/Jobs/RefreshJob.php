e423cca45f1e2bba6df0af28fe76abb3d2a68297
WangNingkai <1655586865@qq.com>
2020-06-17 16:21:15 +0800

    complete command

--- app/Jobs/RefreshJob.php
+++ app/Jobs/RefreshJob.php
@@ -1,5 +1,10 @@
 <?php
-
+/**
+ * This file is part of the wangningkai/olaindex.
+ * (c) wangningkai <i@ningkai.wang>
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
 namespace App\Jobs;
 
 use Illuminate\Bus\Queueable;
@@ -9,7 +14,7 @@ use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
 use Symfony\Component\Process\Process;
 
-class RefreshCache implements ShouldQueue
+class RefreshJob implements ShouldQueue
 {
     use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
 
@@ -20,21 +25,7 @@ class RefreshCache implements ShouldQueue
      */
     public function __construct()
     {
-        $artisan = base_path('artisan');
-        $log = storage_path('app' . DIRECTORY_SEPARATOR . 'refresh.log');
-        $phpPath = config('olaindex.php_path');
-        $isWin = stripos(PHP_OS, 'WIN') === 0;
-        $command = $isWin
-            ? "start /b {$phpPath} {$artisan} od:cache >> {$log}"
-            : "/usr/bin/nohup {$phpPath} {$artisan} od:cache >> {$log}  2>&1 &";
-        $process = new Process($command);
-        $process->run(static function ($type, $buffer) {
-            if (Process::ERR === $type) {
-                echo 'ERR > ' . $buffer;
-            } else {
-                echo 'OUT > ' . $buffer;
-            }
-        });
+
     }
 
     /**
@@ -44,6 +35,20 @@ class RefreshCache implements ShouldQueue
      */
     public function handle()
     {
-        //
+        $artisan = base_path('artisan');
+        $log = storage_path('app' . DIRECTORY_SEPARATOR . 'refresh.log');
+        $phpPath = config('olaindex.php_path');
+        $isWin = stripos(PHP_OS, 'WIN') === 0;
+        $command = $isWin
+            ? "start /b {$phpPath} {$artisan} refresh:data >> {$log}"
+            : "/usr/bin/nohup {$phpPath} {$artisan} refresh:data >> {$log}  2>&1 &";
+        $process = new Process($command);
+        $process->run(static function ($type, $buffer) {
+            if (Process::ERR === $type) {
+                echo 'ERR > ' . $buffer;
+            } else {
+                echo 'OUT > ' . $buffer;
+            }
+        });
     }
 }
