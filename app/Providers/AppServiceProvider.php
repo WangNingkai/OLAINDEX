3bcb3093ee6a7ef1b0eed12023675ef831b3b525
WangNingkai <1655586865@qq.com>
2020-07-06 11:19:54 +0800

    调整服务

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Laravel\Telescope\Telescope;
 use Laravel\Telescope\TelescopeServiceProvider;
 use Schema;
 
@@ -17,7 +18,10 @@ class AppServiceProvider extends ServiceProvider
     {
         if ($this->app->isLocal()) {
             $this->app->register(TelescopeServiceProvider::class);
+        } else {
+            Telescope::ignoreMigrations();
         }
+
     }
 
     /**
