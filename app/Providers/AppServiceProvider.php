6cf6fb0fcd8955661471b8966991a503014f2f5b
WangNingkai <1655586865@qq.com>
2020-07-07 15:42:40 +0800

    安装优化

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -28,5 +28,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+        Schema::defaultStringLength(191);
     }
 }
--- storage/install/data/olaindex.sql
+++ storage/install/data/olaindex.sql
@@ -1,8 +1,116 @@
-CREATE TABLE IF NOT EXISTS "migrations" ("id" integer not null primary key autoincrement, "migration" varchar not null, "batch" integer not null);
-CREATE TABLE IF NOT EXISTS "settings" ("id" integer not null primary key autoincrement, "name" varchar not null, "value" varchar null);
-CREATE TABLE IF NOT EXISTS "users" ("id" integer not null primary key autoincrement, "name" varchar not null, "email" varchar not null, "password" varchar not null, "status" tinyint(1) not null, "created_at" datetime null, "updated_at" datetime null);
-CREATE UNIQUE INDEX "users_email_unique" on "users" ("email");
-INSERT INTO migrations VALUES(1,'2014_10_12_000000_create_users_table',1);
-INSERT INTO migrations VALUES(2,'2019_10_09_143144_create_settings_table',1);
-INSERT INTO users VALUES(1,'admin','admin@qq.com','$2y$10$qWhkwmfsMqdx3.U5jvMYKeHPLUSmp3Yxv1PFQ9wg.Er1BLHyg0.o6',1,NULL,NULL);
+-- --------------------------------------------------------
+-- 主机:                           61.160.251.70
+-- 服务器版本:                        5.5.19-log - Source distribution
+-- 服务器操作系统:                      Linux
+-- HeidiSQL 版本:                  11.0.0.5919
+-- --------------------------------------------------------
 
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET NAMES utf8 */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+
+-- 导出  表 ola.accounts 结构
+CREATE TABLE IF NOT EXISTS `accounts` (
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `remark` varchar(16) COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT '',
+  `accountType` varchar(8) COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT '',
+  `clientId` varchar(128) COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT '',
+  `clientSecret` varchar(128) COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT '',
+  `redirectUri` varchar(128) COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT '',
+  `accessToken` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `refreshToken` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `tokenExpires` int(11) NOT NULL,
+  `status` tinyint(4) NOT NULL DEFAULT '1',
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- 正在导出表  ola.accounts 的数据：~0 rows (大约)
+/*!40000 ALTER TABLE `accounts` DISABLE KEYS */;
+/*!40000 ALTER TABLE `accounts` ENABLE KEYS */;
+
+-- 导出  表 ola.failed_jobs 结构
+CREATE TABLE IF NOT EXISTS `failed_jobs` (
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `connection` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `queue` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `payload` longtext COLLATE utf8mb4_unicode_ci NOT NULL,
+  `exception` longtext COLLATE utf8mb4_unicode_ci NOT NULL,
+  `failed_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- 正在导出表  ola.failed_jobs 的数据：~0 rows (大约)
+/*!40000 ALTER TABLE `failed_jobs` DISABLE KEYS */;
+/*!40000 ALTER TABLE `failed_jobs` ENABLE KEYS */;
+
+-- 导出  表 ola.migrations 结构
+CREATE TABLE IF NOT EXISTS `migrations` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `migration` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `batch` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- 正在导出表  ola.migrations 的数据：~6 rows (大约)
+/*!40000 ALTER TABLE `migrations` DISABLE KEYS */;
+INSERT INTO `migrations` (`id`, `migration`, `batch`) VALUES
+	(1, '2014_10_12_000000_create_users_table', 1),
+	(2, '2018_08_08_100000_create_telescope_entries_table', 1),
+	(3, '2019_08_19_000000_create_failed_jobs_table', 1),
+	(4, '2019_10_09_143144_create_settings_table', 1),
+	(5, '2020_03_29_160103_create_account_table', 1),
+	(6, '2020_06_02_100822_create_short_urls_table', 1);
+/*!40000 ALTER TABLE `migrations` ENABLE KEYS */;
+
+-- 导出  表 ola.settings 结构
+CREATE TABLE IF NOT EXISTS `settings` (
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(64) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `value` text COLLATE utf8mb4_unicode_ci,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- 正在导出表  ola.settings 的数据：~0 rows (大约)
+/*!40000 ALTER TABLE `settings` DISABLE KEYS */;
+/*!40000 ALTER TABLE `settings` ENABLE KEYS */;
+
+-- 导出  表 ola.short_urls 结构
+CREATE TABLE IF NOT EXISTS `short_urls` (
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `original_url` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `short_code` varchar(16) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `idx_code` (`short_code`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- 正在导出表  ola.short_urls 的数据：~0 rows (大约)
+/*!40000 ALTER TABLE `short_urls` DISABLE KEYS */;
+/*!40000 ALTER TABLE `short_urls` ENABLE KEYS */;
+
+-- 导出  表 ola.users 结构
+CREATE TABLE IF NOT EXISTS `users` (
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `email` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `password` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `status` tinyint(1) NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `idx_mail` (`email`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- 正在导出表  ola.users 的数据：~1 rows (大约)
+/*!40000 ALTER TABLE `users` DISABLE KEYS */;
+INSERT INTO `users` (`id`, `name`, `email`, `password`, `status`, `created_at`, `updated_at`) VALUES
+	(1, 'admin', 'admin@qq.com', '$2y$10$VJN/lU/t.Lliug2DmgGLHusZX3TV41qd34V6ZE3XlhZipWxEwzcHi', 1, NULL, NULL);
+/*!40000 ALTER TABLE `users` ENABLE KEYS */;
+
+/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
+/*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
