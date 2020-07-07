fef81d97d66191e7a267770f8aec07ec5ecfdf27
WangNingkai <1655586865@qq.com>
2020-07-07 17:29:07 +0800

    优化

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -28,6 +28,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        Schema::defaultStringLength(191);
+//        Schema::defaultStringLength(191);
     }
 }
