7e943af4137464d802a22f855123485002498223
WangNingkai <1655586865@qq.com>
2020-07-08 09:24:28 +0800

    缩减依赖

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -4,7 +4,6 @@ namespace App\Console;
 
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
-use Spatie\ShortSchedule\ShortSchedule;
 
 class Kernel extends ConsoleKernel
 {
