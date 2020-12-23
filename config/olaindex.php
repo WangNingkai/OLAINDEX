83f65ea2eb0606068369f56156f9fe05d60f6d05
WangNingkai <1655586865@qq.com>
2020-12-23 13:55:41 +0800

    [#] 配置调整

--- config/olaindex.php
+++ config/olaindex.php
@@ -5,7 +5,17 @@
  * This source file is subject to the MIT license that is bundled
  * with this source code in the file LICENSE.
  */
-
+/*
+|--------------------------------------------------------------------------
+| 自定义配置
+|--------------------------------------------------------------------------
+|
+| theme      主题
+| php_path   PHP路径
+| api        API设置
+| proxy      访问代理设置
+|
+*/
 return [
     'theme' => env('THEME', 'default') . '.',
     'php_path' => env('PHP_PATH', '/usr/bin/php'),
