f3611aa72eaf78b41e68da3b8f886c86d98dded1
WangNingkai <1655586865@qq.com>
2020-12-28 08:39:22 +0800

    [#] fix Schema Error

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -24,6 +24,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        Schema::defaultStringLength(191);
+        if (Schema::hasTable('users')) {
+            Schema::defaultStringLength(191);
+        }
     }
 }
